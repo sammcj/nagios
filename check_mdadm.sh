5fb4558e793f07e6a8aca334a9f4525bceb91e93
Sam <sammcj@users.noreply.github.com>
2015-05-21 15:23:57 +1000

    Update check_mdadm.sh

--- check_mdadm.sh
+++ check_mdadm.sh
@@ -20,6 +20,11 @@ function RAID_RECOVER {
   awk '/recovery/{print $0}' /proc/mdstat
 }
 
+# Get the name of any arrays resync
+function RAID_RESYNC {
+  awk '/resync/{print $0}' /proc/mdstat
+}
+
 # The number of arrays
 NUM_ARRAYS=$(RAID_DEVICES | wc -l)
 
@@ -29,6 +34,8 @@ NUM_FAILED=$(FAILED_ARRAYS | wc -l)
 # The number of recovering arrays
 NUM_RECOVER=$(RAID_RECOVER | wc -l)
 
+# The number of recovering arrays
+NUM_RESYNC=$(RAID_RESYNC | wc -l)
 
 # If the number of failed arrays doesn't match the number that are recoverying, return CRITICAL
 if [[ $NUM_RECOVER -ne $NUM_FAILED ]]; then
@@ -38,6 +45,11 @@ if [[ $NUM_RECOVER -ne $NUM_FAILED ]]; then
 # If the only failed arrays are recovering, return WARNING
 elif [[ $NUM_RECOVER -gt 0 ]]; then
   STATUS="$STATUS WARNING - Recovering : $(RAID_RECOVER)"
+  EXIT=1
+
+# If the only failed arrays are recovering, return WARNING
+elif [[ $NUM_RESYNC -gt 0 ]]; then
+  STATUS="$STATUS WARNING - Resyncing : $(RAID_RESYNC)"
   EXIT=1
 
 # If there are no failed arrays return OK
